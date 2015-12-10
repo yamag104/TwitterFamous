a3f927a3e94681ca0aed52cf6e5945c72aaab368
Yoko Yamaguchi <yokoy@Yokos-MacBook-Air.local>
2015-12-10 13:36:25 -0800

    Fixed indentation issue

--- TwitterFamous.py
+++ TwitterFamous.py
@@ -13,68 +13,78 @@ import random
 
 
 class TwitterFamous:
-		def __init__(self):
-				self.config = {}
-				self.connection = None
-				random.seed()
+	def __init__(self):
+		self.config = {}
+		self.connection = None
+		random.seed()
 
-		def setup(self):
-				with open("config.txt", "r") as infile:
-						for line in infile:
-								line = line.split(":")
-								parameter = line[0].strip()
-								value = line[1].strip()
-								self.config[parameter] = value
+	def setup(self):
+		with open("config.txt", "r") as infile:
+			for line in infile:
+				line = line.split(":")
+				parameter = line[0].strip()
+				value = line[1].strip()
+				self.config[parameter] = value
 
-				self.connection = Twitter(auth=OAuth(self.config["OAUTH_TOKEN"],
-																						 self.config["OAUTH_SECRET"],
-																						 self.config["CONSUMER_KEY"],
-																						 self.config["CONSUMER_SECRET"]))
-				print("Your Authenticated.")
-				print(self.connection)
+		self.connection = Twitter(auth=OAuth(self.config["OAUTH_TOKEN"],
+											 self.config["OAUTH_SECRET"],
+											 self.config["CONSUMER_KEY"],
+											 self.config["CONSUMER_SECRET"]))
+		print("Your Authenticated.")
+		print(self.connection)
 
-		def search_tweets(self, word, count, result_type="recent"):
-				return self.connection.search.tweets(q=word, result_type=result_type, count=count)
+	def search_tweets(self, word, count, result_type="recent"):
+		return self.connection.search.tweets(q=word, result_type=result_type, count=count)
 
-		def favorite_tweets(self, word, count, result_type="recent"):
-				result = self.search_tweets(word, count, result_type)
-				for tweet in result["statuses"]:
-						try:
-								result = self.connection.favorites.create(_id=tweet["id"])
-								print("Favorited: %s" % (result["text"].encode("utf-8")), file=sys.stdout)
-						except TwitterHTTPError as api_error:
-								print("ERROR")
+	def favorite_tweets(self, word, count, result_type="recent"):
+		result = self.search_tweets(word, count, result_type)
+		for tweet in result["statuses"]:
+			try:
+				result = self.connection.favorites.create(_id=tweet["id"])
+				print("Favorited: %s" % (result["text"].encode("utf-8")), file=sys.stdout)
+			except TwitterHTTPError as api_error:
+				print("ERROR")
 
-		def retweet(self, word, count, result_type="recent"):
-				result = self.search_tweets(word, count, result_type)
-				for tweet in result["statuses"]:
-						try:
-								result = self.connection.statuses.retweet(id=tweet["id"])
-								print("Retweeted: %s" % (result["text"].encode("utf-8")), file=sys.stdout)
-						except TwitterHTTPError as api_error:
-								print("ERROR")
+	def retweet(self, word, count, result_type="recent"):
+		result = self.search_tweets(word, count, result_type)
+		for tweet in result["statuses"]:
+			try:
+				result = self.connection.statuses.retweet(id=tweet["id"])
+				print("Retweeted: %s" % (result["text"].encode("utf-8")), file=sys.stdout)
+			except TwitterHTTPError as api_error:
+				print("ERROR")
 
-		def follow(self, word, count, result_type="recent"):
-				result = self.search_tweets(word, count, result_type)
-				for tweet in result["statuses"]:
-						try:
-								self.connection.friendships.create(user_id=tweet["user"]["id"], follow=False)
-								print("Followed %s" %
-											(tweet["user"]["screen_name"]), file=sys.stdout)
-						except TwitterHTTPError as api_error:
-								print("ERROR")
+	def follow(self, word, count, result_type="recent"):
+		result = self.search_tweets(word, count, result_type)
+		for tweet in result["statuses"]:
+			try:
+				self.connection.friendships.create(user_id=tweet["user"]["id"], follow=False)
+				print("Followed %s" %
+							(tweet["user"]["screen_name"]), file=sys.stdout)
+			except TwitterHTTPError as api_error:
+				print("ERROR")
 
-		def geo_search(self, latitude, longitude):
-				max_range = 1 			# search range in kilometres
-				try:
-						query = self.connection.search.tweets(q = "", geocode = "%f,%f,%dkm" % (latitude, longitude, max_range), count = 10)
-						for result in query["statuses"]:
-								if result["geo"]:
-										try:
-												self.connection.friendships.create(user_id=result["user"]["id"], follow=False)
-												print("Followed %s" %
-															(result["user"]["screen_name"]), file=sys.stdout)
-										except TwitterHTTPError as api_error:
-												print("ERROR")
-				except Exception:
-						print ("ERROR")
+	def geo_search(self, latitude, longitude):
+		max_range = 1 			# search range in kilometres
+		try:
+			query = self.connection.search.tweets(q = "", geocode = "%f,%f,%dkm" % (latitude, longitude, max_range), count = 10)
+			for result in query["statuses"]:
+				if result["geo"]:
+					try:
+						self.connection.friendships.create(user_id=result["user"]["id"], follow=False)
+						print("Followed %s" %
+									(result["user"]["screen_name"]), file=sys.stdout)
+					except TwitterHTTPError as api_error:
+						print("ERROR")
+		except Exception:
+			print ("ERROR")
+
+	def tweet_rate(self):
+		terms = "pink elephants"
+		query = self.connection.search.tweets(q = terms)
+		results = query["statuses"]
+		# first_timestamp = datetime.strptime(results[0]["created_at"], created_at_format)
+		# last_timestamp = datetime.strptime(results[-1]["created_at"], created_at_format)
+		# total_dt = (first_timestamp - last_timestamp).total_seconds()
+		# mean_dt = total_dt / len(results)
+		# print "Average tweeting rate for '%s' between %s and %s: %.3fs" % (terms, results[-1]["created_at"], results[0]["created_at"], mean_dt)
